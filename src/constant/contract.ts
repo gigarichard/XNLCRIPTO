1b2d8056ffc2fb1bfb2f48cd8b540c16bf7a7836
International Airport Transfers <lnxlofi@gmail.com>
2024-12-26 19:01:42 -0400

    KLK

--- src/constant/contract.ts
+++ src/constant/contract.ts
@@ -7,15 +7,7 @@ export const CONTRACT = {
   ownershipRenounced: true,
   description:
     '$GIGRICHARD, is a memecoin built on the strength of its community. The success of the project is driven by the collective energy, creativity, and support of its members. With the community at its core, this project aims to turn a fun idea into something bigger, proving that together, anything is possible.',
-  partners: [
-    {
-      name: 'PinkSale',
-      url: 'https://www.pinksale.finance',
-      imgUrl: '/pinksale.png',
-    },
-    { name: 'DEXView', url: 'https://www.dexview.com', imgUrl: '/dexview.svg' },
-    { name: 'Binance', url: 'https://www.dexview.com', imgUrl: '/dexview.svg' },
-  ],
+  
   telegram: 'https://t.me/UnKnOwNcHanNeL',
   twitter: 'https://twitter.com/UnKnOwNcHanNeL',
   discord: 'https://discord.com/UnKnOwNcHanNeL',
